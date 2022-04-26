51ad1b86a847c4258e1843647e566015bda87397
admi <letterkbk@gmail.com>
2022-04-26 16:39:00 +0300

    v0.2

--- handlers/user/u_menu.py
+++ handlers/user/u_menu.py
@@ -33,6 +33,7 @@ async def command_call_main_menu(message: types.Message):
                 db.commit()
                 await message.answer(f"Добро пожаловать, Админ: {message.from_user.full_name}", reply_markup=mainMenu)
             else:
+                admin_s = "0"
                 sql_user_reg = "INSERT INTO users (user_id, f_name, l_name, user_status, date ,admin_status) " \
                                "VALUES (%s, %s, %s, %s, %s, %s)"
                 val_user_reg = (message.from_user.id, message.from_user.first_name, message.from_user.last_name, '0',
